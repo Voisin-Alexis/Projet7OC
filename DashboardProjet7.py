68cc595ac03bc26903380851c73e01ff100cc3bb
Voisin-Alexis <bernabe.voisin@laposte.net>
2021-12-08 00:02:05 +0100

    Update DashboardProjet7.py

--- DashboardProjet7.py
+++ DashboardProjet7.py
@@ -404,4 +404,5 @@ def plotfiguregauge(id):
 #==========================================================================================================================#
 
 if __name__ == "__main__":
-    app.run_server(debug = False)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
