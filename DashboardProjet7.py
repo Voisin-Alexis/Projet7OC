503b854c2b49ac60d8791df9033fa6f22e0ed248
Voisin-Alexis <bernabe.voisin@laposte.net>
2021-12-08 01:24:43 +0100

    Update DashboardProjet7.py

--- DashboardProjet7.py
+++ DashboardProjet7.py
@@ -404,4 +404,4 @@ def plotfiguregauge(id):
 #==========================================================================================================================#
 
 if __name__ == "__main__":
-    app.run_server(debug = False)
+    app.run_server(debug = True)
